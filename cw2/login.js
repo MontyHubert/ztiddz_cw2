005c46690a81a95b61de5e999ee4382bd7dd9fe4
Hubert Maciakiewicz <95583560+MontyHubert@users.noreply.github.com>
2023-12-04 13:00:23 +0100

    added comments

--- cw2/login.js
+++ cw2/login.js
@@ -3,15 +3,21 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Zapobieganie domyślnej akcji formularza
+
+    // Pobranie wartości nazwy użytkownika i hasła
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
+    // Sprawdzenie, czy pole nazwa użytkownika i hasło są puste
     if (username === "" && password === "") {
+        // Wyświetlenie alertu o udanym zalogowaniu i przeładowanie strony
         alert("You have successfully logged in.");
         location.reload();
     } else {
+        // Jeśli pole nazwy użytkownika lub hasła nie jest puste, ustawienie widoczności komunikatu o błędzie
         loginErrorMsg.style.opacity = 1;
     }
 })
